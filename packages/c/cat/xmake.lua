8d70bf44139927eb01b6bc3e5916b32f1ce23978
TOMOCAT <TOMO-CAT@github.com>
2024-06-30 17:00:52 +0800

    [Feature]: thread safe for cat rules load

--- packages/c/cat/xmake.lua
+++ packages/c/cat/xmake.lua
@@ -1,12 +1,14 @@
 package("cat", function()
     -- xmake f -c
     on_load(function(package)
+        package:lock()
         os.tryrm(package:installdir("rules"))
         os.cp(path.join(os.scriptdir(), "rules"), package:installdir())
         if option.get("verbose") then
             cprint(
                 "${bright blue}[package@cat][info]${clear} install successfully!")
         end
+        package:unlock()
     end)
 
     on_install(function(package)
