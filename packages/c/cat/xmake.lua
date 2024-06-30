30b6434ded18ae7dd90524c31742a2a0c7b84d4a
TOMOCAT <TOMO-CAT@github.com>
2024-06-30 16:59:59 +0800

    [Feature]: install rules always

--- packages/c/cat/xmake.lua
+++ packages/c/cat/xmake.lua
@@ -3,9 +3,16 @@ package("cat", function()
     on_load(function(package)
         os.tryrm(package:installdir("rules"))
         os.cp(path.join(os.scriptdir(), "rules"), package:installdir())
-        cprint("${bright blue}[package@cat][info]${clear} install successfully!")
+        if option.get("verbose") then
+            cprint(
+                "${bright blue}[package@cat][info]${clear} install successfully!")
+        end
     end)
 
     on_install(function(package)
+        -- handle some bad cases
+        os.vrunv("cp", {
+            "-v", "-r", path.join(os.scriptdir(), "rules"), package:installdir()
+        })
     end)
 end)
