b684b9e23c883da2cfaed0007702fe3b11a41811
TOMOCAT <TOMO-CAT@github.com>
2025-03-22 16:36:06 +0800

    [Feature]: repo rules

---
+++ rules/release_mode.lua
@@ -0,0 +1,26 @@
+rule("release_mode", function()
+    on_config(function(target)
+        if is_mode("release") then
+            -- cuda
+            target:add("cuflags", "-DNDEBUG", {force = true})
+            target:add("culdflags", "-DNDEBUG", {force = true})
+
+            -- c & cpp
+            target:add("cxflags", "-fPIC")
+            if is_arch("arm.*") then
+                target:add("cxflags", "-g")
+                target:add("cxflags", "-gz", "-gdwarf-4")
+                target:add("cxflags", "-DNDEBUG")
+            end
+
+            target:add("ldflags", "-Wl,--export-dynamic", "-Wl,--build-id",
+                       "-flto=thin")
+            target:add("ldflags", "-fuse-ld=lld")
+            -- target:add("shflags", "-fuse-ld=lld")
+
+            if not target:get("optimize") then
+                target:set("optimize", "fastest")
+            end
+        end
+    end)
+end)
